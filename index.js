5d4a9e139f70effbd7dede3240e7fbbca9000b44
Franyeliza <franyelizarodriguez@live.com>
2021-04-04 23:16:05 -0400

    Databse up

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require ('express')
-const CONNECTION_STRING=require('./config')
+const CONNECTION_URL=require('./config')
+var MongoClient = require('mongodb').MongoClient;
 const app = express()
 
 
 const PORT = process.env.PORT || 3000;
+
+
 
 app.get('/', (req, res)=>{
     console.log(req.url)
     res.send("hello")
 })
 
+MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true}, (err, db)=>{
+    if (err){
+        console.log(err)
+    }
+    console.log("conencted")
+})
+    
 app.listen(PORT, ()=>{
     console.log(`listening on port ${PORT}`)
 })
